3b2cc71febca4d5152fe1a8800a48fa0ea3c2875
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2024-06-01 16:36:50 -0500

    Revised style.css and script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+/* Variables */
+
+const selectBox = document.querySelector('.game-start-menu');
+const selectPlayerX = document.querySelector('.icon-x');
+const selectPlayerCircle = document.querySelector('.icon-circle');
+const gameBoard = document.querySelector('#game-board');
+const players = document.querySelectorAll('.pick-players');
+const playerButtons = document.querySelectorAll('.pick-player-button');
+const cellElements = document.querySelectorAll('#game-board-grid section span');
+const gameEndMessage = document.querySelector('#game-end-message');
+const restartGameMessage = document.querySelector('.restart-game-message');
+const gameTiedMessage = document.querySelector('#game-tied-message');
+const newGameVsPlayer = document.querySelector('#new-game-multiplayer');
+const newGameVsPlayerAlert = document.querySelector('#new-game-vs-player-alert');
+const backButton = document.querySelector('.back-button');
+const restartButton = document.querySelector('.restart-button');
+const quitButton = document.querySelector('.quit-button');
+const quitTiedButton = document.querySelector('.quit-tied-button');
+const cancelRestartButton = document.querySelector('.cancel-restart-button');
+const confirmRestartButton = document.querySelector('.confirm-restart-button');
+const nextRoundButton = document.querySelector('.next-round-button');
+const nextRoundTiedButton = document.querySelector('.next-round-tied-button');
+const playerDisplay = document.querySelector('#playerDisplay');
+const newGameButton = document.querySelector('#new-game-solo');
+const headerLarge = document.querySelector('.game-end-header-large');
+const headerSmall = document.querySelector('.game-end-header-small');
+const turnButton = document.querySelector('.turn-button');
+
+/* Event Listeners */
+
+newGameButton.addEventListener('click', startGame);
+newGameVsPlayer.addEventListener('click', startGameVsPlayer);
+quitButton.addEventListener('click', quitGame);
+quitTiedButton.addEventListener('click', quitGame);
+nextRoundButton.addEventListener('click', nextRound);
+nextRoundTiedButton.addEventListener('click', nextRound);
+backButton.addEventListener('click', handleBackButton);
+restartButton.addEventListener('click', restartGame);
+window.addEventListener('resize', centerMainContent);
+
+let playerXScore = 0;
+let playerCircleScore = 0;
+let tiesScore = 0;
+let playerButtonClicked = false;
+let runAi = true;
+
